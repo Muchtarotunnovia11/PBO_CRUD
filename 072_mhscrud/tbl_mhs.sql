f621ffacf48a6f3ae866ec2ee8f410b197cab53b
Muchtarotunnovia11 <92286988+Muchtarotunnovia11@users.noreply.github.com>
2021-11-02 14:44:23 +0700

    Add files via upload

---
+++ 072_mhscrud/tbl_mhs.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 02, 2021 at 08:14 AM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `072_mhscrud`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_mhs`
+--
+
+CREATE TABLE `tbl_mhs` (
+  `id` int(11) NOT NULL,
+  `nim` char(11) NOT NULL,
+  `namamhs` varchar(100) NOT NULL,
+  `jk` enum('L','P') NOT NULL,
+  `alamat` varchar(100) NOT NULL,
+  `kota` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `foto` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_mhs`
+--
+
+INSERT INTO `tbl_mhs` (`id`, `nim`, `namamhs`, `jk`, `alamat`, `kota`, `email`, `foto`) VALUES
+(3, '20051214072', 'MUCHTAROTUN NOVIA USTADHA', 'P', 'DS. TENARU RT 5 RW 2 DRIYOREJO', 'GRESIK', 'ustadhamuchtarotun@gmail.com', '561-Screenshot (47).png');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_mhs`
+--
+ALTER TABLE `tbl_mhs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_mhs`
+--
+ALTER TABLE `tbl_mhs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
